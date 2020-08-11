a7a5e57de7610f2d50dcb4e1d12488a4e6737a00
willwearing <willwearing@gmail.com>
2020-08-11 14:55:09 -0600

    redid the whole project from scratch and have proplery checked its working. both stretch goals completed

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-
-let votingAge = 21;
-
+let votingAge = 19;
 if (votingAge > 18) {
-  console.log("true");
+  console.log(true);
 }
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
-let num1 = 7;
-let num2 = 5;
-
-if (num1 > 5) {
-  num2++;
-  console.log(num2);
+let ranVar = "something";
+let otherVar = "something else";
+if (10 > 0) {
+  ranVar = otherVar;
 }
+
+console.log(ranVar);
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-let string1 = "1999";
-
-let number1 = Number(string1);
-
-console.log(number1);
+console.log(Number("1999"));
 
 //Task d: Write a function to multiply a*b
 
-function myFunction(a, b) {
-  console.log(a * b);
+function multi(a, b) {
+  return a * b;
 }
 
-myFunction(2, 5);
+console.log(multi(2, 3));
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years
-function dogYears(myAge) {
-  console.log(myAge * 7);
+
+function dogYears(age) {
+  return age * 7;
 }
 
-dogYears(30);
+console.log(dogYears(30));
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder
@@ -60,25 +55,24 @@ dogYears(30);
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
 
-function dogFeed(weight, age) {
+function dogFeeder(weight, age) {
   if (age < 4 / 12) {
-    console.log(weight * 0.1);
+    return 0.1 * weight;
   } else if (age < 7 / 12) {
-    console.log(weight * 0.05);
+    return 0.05 * weight;
   } else if (age < 1) {
-    console.log(weight * 0.04);
-  } else if (weight <= 5 && age >= 1) {
-    console.log(weight * 0.05);
-  } else if (weight <= 10 && age >= 1) {
-    console.log(weight * 0.04);
-  } else if (weight <= 15 && age >= 1) {
-    console.log(weight * 0.03);
-  } else {
-    console.log(weight * 0.02);
+    return 0.01 * weight;
+  } else if (age >= 1 && weight <= 5) {
+    return 0.05 * weight;
+  } else if (age >= 1 && weight <= 10) {
+    return 0.04 * weight;
+  } else if (age >= 1 && weight <= 15) {
+    return 0.03 * weight;
+  } else if (age >= 1 && weight > 15) {
+    return 0.02 * weight;
   }
 }
-
-dogFeed(15, 1);
+console.log(dogFeeder(15, 1));
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -87,87 +81,80 @@ dogFeed(15, 1);
 // use math.random to determine the computers choice
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
 
-function winner(choice) {
-  let go1 = choice;
+let computersGo;
 
-  let computer = Math.random();
-
-  console.log(computer);
-
-  let computerGo;
-
-  if (computer < 0.33) {
-    computerGo = "rock";
-  } else if (computer < 0.66) {
-    computerGo = "paper";
-  } else {
-    computerGo = "scissors";
+function computer(max) {
+  let computersAnswer = Math.floor(Math.random() * Math.floor(max));
+  if (computersAnswer == 0) {
+    computersGo = "rock";
+  } else if (computersAnswer == 1) {
+    computersGo = "paper";
+  } else if (computersAnswer == 2) {
+    computersGo = "scissors";
   }
-
-  console.log(computerGo);
-  let result;
-
-  if (go1 == "rock" && computerGo == "rock") {
-    result = "draw";
-  } else if (go1 == "rock" && computerGo == "paper") {
-    result = "computer wins";
-  } else if (go1 == "rock" && computerGo == "scissors") {
-    result = "human wins";
-  } else if (go1 == "paper" && computerGo == "paper") {
-    result = "draw";
-  } else if (go1 == "paper" && computerGo == "rock") {
-    result = "human wins";
-  } else if (go1 == "paper" && computerGo == "scissors") {
-    result = "computer wins";
-  } else if (go1 == "scissors" && computerGo == "scissors") {
-    result = "draw";
-  } else if (go1 == "scissors" && computerGo == "rock") {
-    result = "computer wins";
-  } else if (go1 == "scissors" && computerGo == "paper") {
-    result = "human wins";
-  }
-
-  return result;
+  return computersAnswer;
 }
 
-console.log(winner("rock"));
+computer(3);
+
+function rockPaperScissors(myGo, computersGo) {
+  if (myGo == "rock" && computersGo == "paper") {
+    return "computer wins";
+  } else if (myGo == "rock" && computersGo == "scissors") {
+    return "you win";
+  } else if (myGo == "rock" && computersGo == "rock") {
+    return "draw";
+  } else if (myGo == "paper" && computersGo == "rock") {
+    return "you win";
+  } else if (myGo == "paper" && computersGo == "scissors") {
+    return "computer wins";
+  } else if (myGo == "paper" && computersGo == "paper") {
+    return "draw";
+  } else if (myGo == "scissors" && computersGo == "rock") {
+    return "computer wins";
+  } else if (myGo == "scissors" && computersGo == "paper") {
+    return "you win";
+  } else if (myGo == "scissors" && computersGo == "scissors") {
+    return "draw";
+  }
+}
+
+console.log(rockPaperScissors("rock", computersGo));
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+function converterKM(km) {
+  return km * 0.621371;
+}
+
+console.log(converterKM(10));
+
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
 
-function kmMiles(km) {
-  let miles = km * 0.62;
-  console.log(miles);
+function converterIN(inch) {
+  return inch * 2.54;
 }
 
-kmMiles(10);
-
-function feetCM(feet) {
-  let cm = feet * 0.03;
-  console.log(cm);
-}
-
-feetCM(3);
+console.log(converterIN(10));
 
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
 
-function annoyingSong(num1) {
-  for (let index = num1; index > 0; index--) {
+function annoyingSong(startNum) {
+  for (let i = startNum; i > 0; i--) {
     console.log(
-      `${index} bottles of soda, ${index} bottles of soda, take one down pass it around ${
-        index - 1
+      `${i} bottles of soda, ${i} bottles of soda, take one down pass it around, ${
+        i - 1
       } bottles of soda on the wall`
     );
   }
 }
 
-annoyingSong(10);
+annoyingSong(9);
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
@@ -178,23 +165,21 @@ annoyingSong(10);
 //60s should be D
 //and anything below 60 should be F
 
-function whatIs(score) {
-  let grade;
-  if (score >= 90) {
-    grade = "A";
-  } else if (score >= 80) {
-    grade = "B";
-  } else if (score >= 70) {
-    grade = "C";
-  } else if (score >= 60) {
-    grade = "D";
-  } else {
-    grade = "F";
+function gradeCalc(mark) {
+  if (mark >= 90) {
+    return "A";
+  } else if (mark >= 80) {
+    return "B";
+  } else if (mark >= 70) {
+    return "C";
+  } else if (mark >= 60) {
+    return "D";
+  } else if (mark < 60) {
+    return "F";
   }
-  console.log(grade);
 }
 
-whatIs(91);
+console.log(gradeCalc(70));
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
@@ -203,20 +188,60 @@ whatIs(91);
 
 let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-function countVowels(str) {
+function countVowels(text) {
   let count = 0;
-
-  for (let letters of str) {
+  for (let letters of text) {
     if (vowels.includes(letters)) {
       count++;
     }
   }
-
   console.log(count);
-
 }
 
-countVowels("My names William Wearing");
+countVowels("William");
+
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
+
+let playerChoice = prompt("Type in one of the follow: rock, paper or scissors");
+
+let computersGo1;
+
+function computer1(max1) {
+  let computersAnswer1 = Math.floor(Math.random() * Math.floor(max1));
+  if (computersAnswer1 == 0) {
+    computersGo1 = "rock";
+  } else if (computersAnswer1 == 1) {
+    computersGo1 = "paper";
+  } else if (computersAnswer1 == 2) {
+    computersGo1 = "scissors";
+  }
+  return computersAnswer1;
+}
+
+computer1(3);
+
+function rockPaperScissors1(myGo1, computersGo1) {
+  if (myGo1 == "rock" && computersGo1 == "paper") {
+    return "computer wins";
+  } else if (myGo1 == "rock" && computersGo1 == "scissors") {
+    return "you win";
+  } else if (myGo1 == "rock" && computersGo1 == "rock") {
+    return "draw";
+  } else if (myGo1 == "paper" && computersGo1 == "rock") {
+    return "you win";
+  } else if (myGo1 == "paper" && computersGo1 == "scissors") {
+    return "computer wins";
+  } else if (myGo1 == "paper" && computersGo1 == "paper") {
+    return "draw";
+  } else if (myGo1 == "scissors" && computersGo1 == "rock") {
+    return "computer wins";
+  } else if (myGo1 == "scissors" && computersGo1 == "paper") {
+    return "you win";
+  } else if (myGo1 == "scissors" && computersGo1 == "scissors") {
+    return "draw";
+  }
+}
+
+console.log(rockPaperScissors1(playerChoice, computersGo1));
